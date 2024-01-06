bce3257124bb58324f396566525ca6e859545072
Roshan Gurung <grgroshan809@gmail.com>
2024-01-06 23:31:01 +0000

    Code completed

--- User Interface/collaborative_filtering.py
+++ User Interface/collaborative_filtering.py
@@ -1,13 +1,3 @@
-import pickle
-import pandas as pd
-
-
-class CollaborativeFilteringModel:
-    # declare file path for saved model
-    file_path = '../final_model_svd.pkl'
-
-    # function to execute user-based Collaborative Filtering
-    def load_model (self):
-        loaded_model = pickle.load(open(self.file_path, 'rb'))
-
-        return loaded_model
+version https://git-lfs.github.com/spec/v1
+oid sha256:11c5707bdbcbd143f98830b0891ed9b2dddb62d01d31bd2a880f3fb724012fb6
+size 347
--- User Interface/movie_profile.py
+++ User Interface/movie_profile.py
@@ -1,19 +1,3 @@
-import streamlit as st
-import pandas as pd
-
-def display_movie_profile(movie_id):
-    # load the movie dataset
-    # movies=pd.read_csv('../Movielens dataset/movies.csv')
-    #
-    # # find the dmovie details based on movie_id
-    # movie_details=movies[movies['movieId']==movie_id]
-    #
-    # # display the movie details
-    # st.title(movie_details['title'].values[0])
-    # st.write(f"Genres:")
-    st.write("Welcome")
-
-
-def display_movie_profile_page(selected_movie_id):
-    st.title('Movie Profile')
-    display_movie_profile(selected_movie_id)
+version https://git-lfs.github.com/spec/v1
+oid sha256:b6f511f87a8ba04908b4fefe5b74df1d5f1bfa88047d1a286b07e4eab07958f7
+size 549
