6d4045bf8287654222d2e687c34d1399bb872018
Roshan Gurung <92009687+RoshanGurung123@users.noreply.github.com>
2024-01-07 12:47:23 +0000

    deleting unecessary files

--- user_profile.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-# function for user_profile
-# reason to create user_profile is to show about the user how many movies the user have rated so far
-def user_profile():
-    movie_details=
